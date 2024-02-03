a34692774464460f82d15623ed8beef65b76b089
sjc230 <70530110+sjc230@users.noreply.github.com>
2024-02-03 15:02:52 -0500

    mod

--- sk_grid_builder.py
+++ sk_grid_builder.py
@@ -69,7 +69,8 @@ def gridsearch_clustering(names,pipes,X,y,scoring='rand_score'):
       noise = np.isin(labels, -1)
       if np.any(noise)==True:
          new_noise_label = int(np.amax(labels)+1) # find the max label value
-         labels[labels == -1] = str(new_noise_label) # find all noise labels (-1 values) & replace with a new positive value
+         #labels[labels == -1] = str(new_noise_label) # find all noise labels (-1 values) & replace with a new positive value
+         labels = np.char.replace(labels, '-1', new_noise_label)
          
 
       x_classes = int(np.amax(labels)+1)
