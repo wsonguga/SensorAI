3e9636a905c1a85274b2a8d1680e8a78dcf1e74f
sjc230 <70530110+sjc230@users.noreply.github.com>
2024-03-03 18:34:25 -0500

    trace color update

--- classification.py
+++ classification.py
@@ -3697,7 +3697,7 @@ def gridsearch_classifier(names,pipes,X_train,X_test,y_train,y_test,scoring='neg
                         else:
                             color = 'red'
                         fig.add_trace(
-                            go.Scatter(x=x_axis,y=X_test[count],color=color),
+                            go.Scatter(mode='markers',x=x_axis,y=X_test[count],marker=dict(color=color)),
                             row=plot_num+1, col=current_label+1
                         )                       
                         plot_num = plot_num +1
