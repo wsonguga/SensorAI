e1ae6fe54427f728c3d99b019594705123e2618d
sjc230 <70530110+sjc230@users.noreply.github.com>
2024-08-15 13:16:56 -0400

    tcn corrections

--- deeplearning.py
+++ deeplearning.py
@@ -34,7 +34,9 @@ def pipeBuild_TCN(num_inputs=[8],num_channels=[[4]],kernel_size=[4],dilations=[N
                   activation=['relu'],kernel_initializer=['xavier_uniform'],use_skip_connections=[False],
                   input_shape=['NCL'],embedding_shapes=[None],embedding_mode=['add'],use_gate=[False],
                   lookahead=[1],output_projection=[None],output_activation=[None]): 
-    classifier = TCN()
+    classifier = TCN(num_inputs,num_channels,kernel_size,dilations,dilation_reset,dropout,causal,use_norm,
+                     activation,kernel_initializer,use_skip_connections,input_shape,embedding_shapes,
+                     embedding_mode,use_gate,lookahead,output_projection,output_activation)
     pipeline = Pipeline(steps=[('tcn', classifier)])
     params = [{
         'tcn__num_inputs': num_inputs,
