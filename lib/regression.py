6891f75ff4d40be5909a1d0cb22bb6248e9fa292
sjc230 <70530110+sjc230@users.noreply.github.com>
2025-03-11 14:19:22 -0400

    mlp fix

--- lib/regression.py
+++ lib/regression.py
@@ -571,7 +571,7 @@ def pipeBuild_LassoLarsIC(criterion=['aic'], *, fit_intercept=[True], verbose=[F
 
 # MLP REGRESSPR
 def pipeBuild_MLPRegressor(hidden_layer_sizes=[(100,)], activation=['relu'], alpha=[0.0001], batch_size=['auto'], learning_rate=['constant'], learning_rate_init=[0.001], 
-                         power_t=[0.5], max_iter=[200], random_state=0, tol=[0.001], shuffle=[True], verbose=[0], warm_start=[False], momentum=[0.9], nesterovs_momentum=[True], 
+                         power_t=[0.5], max_iter=[200], random_state=None, tol=[0.001], shuffle=[True], verbose=[0], warm_start=[False], momentum=[0.9], nesterovs_momentum=[True], 
                          early_stopping=[False], validation_fraction=[0.1], beta_1=[0.9], beta_2=[0.999], epsilon=[1e-08], n_iter_no_change=[5],
                          max_fun=[15000]):
   regressor = MLPRegressor(random_state=random_state)
